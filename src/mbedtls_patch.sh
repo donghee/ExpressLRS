6005579381e69667ced692ee5e10d07345b99af1
Donghee Park <dongheepark@gmail.com>
2024-03-17 21:44:21 +0900

    Fix compile error to rename the mbedtls library file which conflict with expresslrs header file

--- src/mbedtls_patch.sh
+++ src/mbedtls_patch.sh
@@ -1,1 +1,2 @@
 cp mbedtls_config.h .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/include/mbedtls/config.h
+mv .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/library/common.h .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/library/mbedtls_common.h
