783b4741dcbe22f8f1bd9dc7cb2fb1feca01f3e8
Donghee Park <dongheepark@gmail.com>
2024-04-01 13:04:22 +0900

    WIP: encrypt lea key using pubkey from handshake

--- src/mbedtls_patch.sh
+++ src/mbedtls_patch.sh
@@ -1,2 +1,5 @@
 cp mbedtls_config.h .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/include/mbedtls/config.h
+cp mbedtls_config.h .pio/libdeps/LEA_2400_RX_GCM_TEST/mbedtls/include/mbedtls/config.h
+
 mv .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/library/common.h .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/library/mbedtls_common.h
+mv .pio/libdeps/LEA_2400_RX_GCM_TEST/mbedtls/library/common.h .pio/libdeps/LEA_2400_RX_GCM_TEST/mbedtls/library/mbedtls_common.h
--- src/src/rx_gcm_main.cpp
+++ src/src/rx_gcm_main.cpp
@@ -4,6 +4,7 @@
 #include "common.h"
 #include "FHSS.h"
 #include "gcm.h"
+#include "rsa.h"
 
 #define assert(c) if (!(c)) __BKPT()
 
@@ -34,7 +35,6 @@ void ICACHE_RAM_ATTR TXdoneCallback()
 
 bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const status)
 {
-
     // uint8_t plaintext[DATA_SIZE];
     // int ret = 0;
     // ret = lea_gcm.decrypt((OTA_Packet_s *) plaintext, (const uint8_t *) Radio.RXdataBuffer, 32);
@@ -49,19 +49,61 @@ bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const status)
         digitalWrite(GPIO_PIN_LED, !digitalRead(GPIO_PIN_LED));
         handshake_state = HANDSHAKE_WAIT_RSA_PUBKEY;
         packageIndex = 0;
+        pubkey_len = 0;
     } else if (handshake_state == HANDSHAKE_WAIT_RSA_PUBKEY) {
         if (packageIndex >= 32) {
             handshake_state = HANDSHAKE_DONE;
-            __BKPT();
-            memset(pubkey, 0, sizeof(pubkey));
+            // __BKPT();
+            // memset(pubkey, 0, sizeof(pubkey));
             return true;
         }
         memcpy(pubkey + (packageIndex * 8), Radio.RXdataBuffer, 8);
         packageIndex++;
+        pubkey_len += 8;
     }
 
     Radio.RXnb();
     return true;
+}
+
+void rsa_encrypt_test()
+{
+    int ret = 1;
+    const char *pers = "rsa_genkey";
+
+    unsigned char plaintext_pub[1024] = {0};
+    unsigned char ciphertext_pub[512] = {0};
+
+    // generate plaintext for test
+    uint8_t K[16] = {0x14, 0x87, 0x0B, 0x99, 0x92, 0xEA, 0x89, 0x67, 0x8A, 0x1D, 0xDF, 0xD6, 0x30, 0x91, 0x8D, 0xF0};
+    uint8_t A[16] = {0};
+    uint8_t N[12] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B};
+
+    memcpy(plaintext_pub, K, 16);
+    memcpy(plaintext_pub + 16, A, 16);
+    memcpy(plaintext_pub + 32, N, 12);
+
+    RSA rsa;
+    // generate key for test
+    ret = rsa.generate_key(pers, strlen(pers));
+    if ( ret != 0 )
+        DBGLN("FAILED GENERATE KEY");
+
+    // encrypt using generated public key
+    ret = rsa.encrypt(pubkey, pubkey_len, plaintext_pub, ciphertext_pub, 16+16+12);
+    if ( ret != 0 ) {
+        __BKPT();
+        return;
+    }
+
+    // decrypt for test
+    unsigned char decryptedtext[1024] = {0};
+    size_t i;
+    ret = rsa.decrypt(ciphertext_pub, decryptedtext, &i, 1024);
+    if( ret != 0 )
+        DBGLN("FAILED DECRYPT");
+
+    __BKPT();
 }
 
 void setup()
@@ -88,6 +130,7 @@ void setup()
 void loop()
 {
     if (handshake_state == HANDSHAKE_DONE) {
+        rsa_encrypt_test();
         handshake_state = HANDSHAKE_WAIT_HELLO;
         Radio.RXnb();
     }
