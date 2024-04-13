1f4b208a0a69e911facf02b8f72fe0c6bc7cd998
Donghee Park <dongheepark@gmail.com>
2024-04-14 00:47:31 +0900

    Add mbedtls library

--- src/mbedtls_patch.sh
+++ src/mbedtls_patch.sh
@@ -1,5 +1,13 @@
+# gcm test
 cp mbedtls_config.h .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/include/mbedtls/config.h
 cp mbedtls_config.h .pio/libdeps/LEA_2400_RX_GCM_TEST/mbedtls/include/mbedtls/config.h
 
 mv .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/library/common.h .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/library/mbedtls_common.h
 mv .pio/libdeps/LEA_2400_RX_GCM_TEST/mbedtls/library/common.h .pio/libdeps/LEA_2400_RX_GCM_TEST/mbedtls/library/mbedtls_common.h
+
+# main 
+cp mbedtls_config.h .pio/libdeps/LEA_2400_TX_via_STLINK/mbedtls/include/mbedtls/config.h
+cp mbedtls_config.h .pio/libdeps/LEA_2400_RX_via_STLINK/mbedtls/include/mbedtls/config.h
+
+mv .pio/libdeps/LEA_2400_TX_via_STLINK/mbedtls/library/common.h .pio/libdeps/LEA_2400_TX_via_STLINK/mbedtls/library/mbedtls_common.h
+mv .pio/libdeps/LEA_2400_RX_via_STLINK/mbedtls/library/common.h .pio/libdeps/LEA_2400_RX_via_STLINK/mbedtls/library/mbedtls_common.h
