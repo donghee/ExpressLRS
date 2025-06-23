db125698cc697dcb53add254ed226e5acb24fe8e
Donghee Park <dongheepark@gmail.com>
2025-06-23 09:16:18 +0900

    Add LEA AIO TX board profile

--- src/build.sh
+++ src/build.sh
@@ -9,8 +9,15 @@ $HOME/.platformio/penv/bin/pio run -e LEA_2400_TX_via_STLINK --target clean && $
 	"RX")
 $HOME/.platformio/penv/bin/pio run -e LEA_2400_RX_via_STLINK --target clean && $HOME/.platformio/penv/bin/pio debug -e LEA_2400_RX_via_STLINK
     ;;
+  "AIO_TX")
+$HOME/.platformio/penv/bin/pio run -e LEA_2400_AIO_TX_via_STLINK --target clean && $HOME/.platformio/penv/bin/pio debug -e LEA_2400_AIO_TX_via_STLINK
+    ;;
+	"AIO_RX")
+$HOME/.platformio/penv/bin/pio run -e LEA_2400_AIO_RX_via_STLINK --target clean && $HOME/.platformio/penv/bin/pio debug -e LEA_2400_AIO_RX_via_STLINK
+    ;;
+
 	 *)
-     echo "TX or RX"
+     echo "TX or RX or AIO_TX or AIO_RX must be specified as an argument."
      exit 1
      ;;
 esac
--- src/mbedtls_patch.sh
+++ src/mbedtls_patch.sh
@@ -5,9 +5,18 @@ cp mbedtls_config.h .pio/libdeps/LEA_2400_RX_GCM_TEST/mbedtls/include/mbedtls/co
 mv .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/library/common.h .pio/libdeps/LEA_2400_TX_GCM_TEST/mbedtls/library/mbedtls_common.h
 mv .pio/libdeps/LEA_2400_RX_GCM_TEST/mbedtls/library/common.h .pio/libdeps/LEA_2400_RX_GCM_TEST/mbedtls/library/mbedtls_common.h
 
-# main 
+# main
 cp mbedtls_config.h .pio/libdeps/LEA_2400_TX_via_STLINK/mbedtls/include/mbedtls/config.h
 cp mbedtls_config.h .pio/libdeps/LEA_2400_RX_via_STLINK/mbedtls/include/mbedtls/config.h
 
 mv .pio/libdeps/LEA_2400_TX_via_STLINK/mbedtls/library/common.h .pio/libdeps/LEA_2400_TX_via_STLINK/mbedtls/library/mbedtls_common.h
 mv .pio/libdeps/LEA_2400_RX_via_STLINK/mbedtls/library/common.h .pio/libdeps/LEA_2400_RX_via_STLINK/mbedtls/library/mbedtls_common.h
+
+# aio main
+cp mbedtls_config.h .pio/libdeps/LEA_2400_AIO_TX_via_STLINK/mbedtls/include/mbedtls/config.h
+cp mbedtls_config.h .pio/libdeps/LEA_2400_AIO_RX_via_STLINK/mbedtls/include/mbedtls/config.h
+
+mv .pio/libdeps/LEA_2400_AIO_TX_via_STLINK/mbedtls/library/common.h .pio/libdeps/LEA_2400_AIO_TX_via_STLINK/mbedtls/library/mbedtls_common.h
+mv .pio/libdeps/LEA_2400_AIO_RX_via_STLINK/mbedtls/library/common.h .pio/libdeps/LEA_2400_AIO_RX_via_STLINK/mbedtls/library/mbedtls_common.h
+
+
