02fb4849d6b3bac93c52b7348e8c8b95bca5b3ed
Donghee Park <dongheepark@gmail.com>
2025-07-04 07:23:05 +0900

    Add LEA AIO RX board config

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
-.PHONY: install-tools
+.PHONY: setup
 
-setup:
+build-docker-image:
+	./docker/build.sh
+
+run-docker-container:
+	./docker/run.sh
+
+setup: build-docker-image
 	sudo apt-get update -y && sudo apt-get install gdb-multiarch libusb-1.0.0-dev -y
 	pip3 install --no-cache-dir platformio --user --break-system-packages
+	ln -s ~/.platformio/penv/bin/platformio ~/.local/bin/pio
 	pip3 install -U pyocd --user --break-system-packages
 	sudo cp docker/udev/*.rules /etc/udev/rules.d
 	sudo udevadm control --reload
@@ -10,7 +17,7 @@ setup:
 
 prebuild:
 	sudo chown -R `id -un` src/.pio/build
-	#git pull --rebase
+	# git pull --rebase --autostash
 	
 build-tx: prebuild
 	cd src; ./mbedtls_patch.sh; pio run -t clean -e LEA_2400_TX_via_STLINK && pio debug -e LEA_2400_TX_via_STLINK
@@ -19,14 +26,22 @@ build-rx: prebuild
 	cd src; ./mbedtls_patch.sh; pio run -t clean -e LEA_2400_RX_via_STLINK && pio debug -e LEA_2400_RX_via_STLINK
 
 build-aio-tx: prebuild
-	cd src; ./mbedtls_patch.sh; pio run -t clean -e LEA_2400_AIO_RX_via_STLINK && pio debug -e LEA_2400_AIO_RX_via_STLINK
+	cd src; ./mbedtls_patch.sh; pio run -t clean -e LEA_2400_AIO_TX_via_STLINK && pio debug -e LEA_2400_AIO_TX_via_STLINK
 
 build-aio-rx: prebuild
 	cd src; ./mbedtls_patch.sh; pio run -t clean -e LEA_2400_AIO_RX_via_STLINK && pio debug -e LEA_2400_AIO_RX_via_STLINK
 
-build-docker-image:
-	./docker/build.sh
-
 build-using-container:
 	docker exec ExpressLRS bash -c 'cd /workspaces/ExpressLRS/src; ./mbedtls_patch.sh; pio run -t clean -e LEA_2400_AIO_TX_via_STLINK'
 	docker exec ExpressLRS bash -c 'cd /workspaces/ExpressLRS/src; ./mbedtls_patch.sh; pio debug -e LEA_2400_AIO_TX_via_STLINK'
+
+build: build-tx build-rx build-aio-tx build-aio-rx
+	@echo 'Successfully built ExpressLRS TX and RX firmwares'
+
+debug-aio-tx: build-aio-tx
+	@echo 'pyocd gdbserver --persist -t stm32f479iihx -u 003800423433510837363934 -p 3333 -T 4444 # AIO TX STLINK-V3'
+	cd src; gdb-multiarch -x .pioinit-aio-tx
+
+debug-aio-rx: build-aio-rx
+	@echo 'pyocd gdbserver --persist -t stm32f479iihx -u 004400353133510537363734 -p 3334 -T 4445 # AIO RX STLINK-V3'
+	cd src; gdb-multiarch -x .pioinit-aio-rx
---
+++ src/include/target/LEA_2400_AIO_RX.h
@@ -0,0 +1,60 @@
+#if !defined(DEVICE_NAME)
+    #define DEVICE_NAME             "LEA AIO RX"
+#endif
+// There is some special handling for this target
+#define TARGET_AIO_RX_LEA
+
+#define HAL_SPI_MODULE_ENABLED
+
+// Any device features
+// #if !defined(USE_OLED_SPI_SMALL)
+//     #define USE_OLED_SPI
+// #endif
+
+// #define HAS_FIVE_WAY_BUTTON
+
+// GPIO pin definitions
+#define GPIO_PIN_NSS                PI0
+#define GPIO_PIN_BUSY               PG13
+#define GPIO_PIN_DIO1               PG14
+#define GPIO_PIN_MOSI               PB15
+#define GPIO_PIN_MISO               PB14
+#define GPIO_PIN_SCK                PI1
+#define GPIO_PIN_RST                PB5
+// #define GPIO_PIN_TX_ENABLE          PA2  // Works on Lite
+// #define GPIO_PIN_RX_ENABLE          PA3 // Works on Lite
+// #define GPIO_PIN_ANT_CTRL           PA9
+// #define GPIO_PIN_ANT_CTRL_COMPL     PB13
+// #define GPIO_PIN_RCSIGNAL_RX        PA10 // S.PORT (Only needs one wire )
+// #define GPIO_PIN_RCSIGNAL_TX        PA9  // Needed for CRSF libs but does nothing/not hooked up to JR module.
+#define GPIO_PIN_RCSIGNAL_RX        PD2
+#define GPIO_PIN_RCSIGNAL_TX        PC12
+
+#define GPIO_PIN_LED            PG9
+//#define GPIO_PIN_LED_WS2812         PB_8
+//#define GPIO_PIN_LED_WS2812_FAST    PB_6
+// #define GPIO_PIN_PA_ENABLE          PB11  // https://www.skyworksinc.com/-/media/SkyWorks/Documents/Products/2101-2200/SE2622L_202733C.pdf
+// #define GPIO_PIN_RF_AMP_DET         PA3  // Voltage detector pin
+//#define GPIO_PIN_BUZZER             PB_7
+// #define GPIO_PIN_OLED_CS            PC14
+// #define GPIO_PIN_OLED_RST           PB12
+// #define GPIO_PIN_OLED_DC            PC15
+// #define GPIO_PIN_OLED_MOSI          PB5
+// #define GPIO_PIN_OLED_SCK           PB3
+// #define GPIO_PIN_JOYSTICK           A0
+
+// #define GPIO_PIN_DEBUG_RX           PA_3 // WTF! It's used for AMP_DET
+// #define GPIO_PIN_DEBUG_TX           PA_2
+
+#define GPIO_PIN_DEBUG_RX           PC11 // UART 4
+#define GPIO_PIN_DEBUG_TX           PC10 // UART 4
+
+// Output Power
+#define MinPower                    PWR_10mW
+#define MaxPower                    PWR_250mW
+#define POWER_OUTPUT_VALUES         {-16,-14,-11,-8,-4}
+
+// #ifndef JOY_ADC_VALUES
+// [> Joystick values              {UP, DOWN, LEFT, RIGHT, ENTER, IDLE}<]
+// #define JOY_ADC_VALUES          {459, 509, 326, 182, 91, 1021}
+// #endif
